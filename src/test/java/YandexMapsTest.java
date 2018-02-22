8c985e1025fcdc40e87a0359f4d9aa0d8d730c78
Nikolai_Kelin <kelin666@mail.ru>
2018-02-22 08:59:06 +0300

    fixed some mistakes

--- src/test/java/YandexMapsTest.java
+++ src/test/java/YandexMapsTest.java
@@ -4,8 +4,6 @@ import org.apache.http.HttpStatus;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 
-import java.util.Arrays;
-
 import static core.YandexMapsContacts.*;
 import static org.hamcrest.Matchers.lessThan;
 
@@ -22,13 +20,10 @@ public class YandexMapsTest {
                 .get(YANDEX_MAPS_API_URI)
                 .prettyPeek()
                 .then()
-
                 .assertThat()
                 .statusCode(HttpStatus.SC_OK)
                 .body(Matchers.allOf(
-                        Matchers.stringContainsInOrder(Arrays.asList("Россия"))))
-                // Matchers.containsString("\"code\":1")))
-                //  .contentType(ContentType.JSON)
+                        Matchers.containsString("Россия")))
                 .time(lessThan(20000L)); // Milliseconds
     }
 
@@ -53,5 +48,53 @@ public class YandexMapsTest {
                 .prettyPeek();
         System.out.println("\n=====================================================================");
 
+        //HEAD
+        RestAssured
+                .given()
+                .param(GEOCODE, PLACE)
+                .log().everything()
+                .head(YANDEX_MAPS_API_URI)
+                .prettyPeek();
+        System.out.println("\n=====================================================================");
+
+        //OPTIONS
+        RestAssured
+                .given()
+                .param(GEOCODE, PLACE)
+                .log().everything()
+                .options(YANDEX_MAPS_API_URI)
+                .prettyPeek();
+        System.out.println("\n=====================================================================");
+
+        //PUT
+        RestAssured
+                .given()
+                .param(GEOCODE, PLACE)
+                .log().everything()
+                .put(YANDEX_MAPS_API_URI)
+                .prettyPeek();
+        System.out.println("\n=====================================================================");
+
+        //PATCH
+        RestAssured
+                .given()
+                .param(GEOCODE, PLACE)
+                .log()
+                .everything()
+                .patch(YANDEX_MAPS_API_URI)
+                .prettyPeek();
+        System.out.println("\n=====================================================================");
+
+        //DELETE
+        RestAssured
+                .given()
+                .param(GEOCODE, PLACE)
+                .log()
+                .everything()
+                .delete(YANDEX_MAPS_API_URI).prettyPeek()
+                .then()
+                .statusCode(HttpStatus.SC_METHOD_NOT_ALLOWED)
+                .statusLine("HTTP/1.1 405 Not Allowed");
     }
 }
+
